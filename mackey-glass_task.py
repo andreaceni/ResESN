5a0717d9ce5007533a98ff9f38fe7c3c677499bd
andreaceni <99392616+andreaceni@users.noreply.github.com>
2024-06-10 16:33:01 +0200

    Update mackey-glass_task.py

--- mackey-glass_task.py
+++ mackey-glass_task.py
@@ -4,26 +4,14 @@ import argparse
 from esn import DeepReservoir
 from sklearn import preprocessing
 from sklearn.linear_model import Ridge
-from utils import get_mackey_glass, coESN, check
+from utils import get_mackey_glass
 
 
 parser = argparse.ArgumentParser(description='training parameters')
 
 parser.add_argument('--n_hid', type=int, default=256,
                     help='hidden size of recurrent net')
-parser.add_argument('--dt', type=float, default=0.076,
-                    help='step size <dt> of the coRNN')
-parser.add_argument('--gamma', type=float, default=0.4,
-                    help='y controle parameter <gamma> of the coRNN')
-parser.add_argument('--epsilon', type=float, default=8.0,
-                    help='z controle parameter <epsilon> of the coRNN')
-parser.add_argument('--gamma_range', type=float, default=2.7,
-                    help='y controle parameter <gamma> of the coRNN')
-parser.add_argument('--epsilon_range', type=float, default=4.7,
-                    help='z controle parameter <epsilon> of the coRNN')
 parser.add_argument('--cpu', action="store_true")
-parser.add_argument('--check', action="store_true")
-parser.add_argument('--no_friction', action="store_true")
 parser.add_argument('--inp_scaling', type=float, default=1.,
                     help='ESN input scaling')
 parser.add_argument('--rho', type=float, default=0.99,
@@ -100,17 +88,13 @@ n_out = 1
 washout = 200
 lag = args.lag
 
-gamma = (args.gamma - args.gamma_range / 2., args.gamma + args.gamma_range / 2.)
-epsilon = (args.epsilon - args.epsilon_range / 2., args.epsilon + args.epsilon_range / 2.)
-
 (train_dataset, train_target), (valid_dataset, valid_target), (test_dataset, test_target) = get_mackey_glass(args.lag)
 
 NRMSE = np.zeros(args.test_trials)
 for guess in range(args.test_trials):
 
-    if not args.no_friction:
-        if args.Euler:
-            model = DeepReservoir(n_inp, tot_units=args.n_hid, spectral_radius=args.rho,
+    if args.Euler:
+        model = DeepReservoir(n_inp, tot_units=args.n_hid, spectral_radius=args.rho,
                                 input_scaling=args.inp_scaling,
                                 connectivity_recurrent=args.n_hid,
                                 connectivity_input=args.n_hid, 
@@ -125,8 +109,8 @@ for guess in range(args.test_trials):
                                 gamma=args.diffus,
                                 recur_scaling=args.EuRec_scal,
                                 ).to(device)
-        else:
-            model = DeepReservoir(n_inp, tot_units=args.n_hid, spectral_radius=args.rho,
+    else:
+        model = DeepReservoir(n_inp, tot_units=args.n_hid, spectral_radius=args.rho,
                                 input_scaling=args.inp_scaling,
                                 connectivity_recurrent=args.n_hid,
                                 connectivity_input=args.n_hid, 
@@ -138,42 +122,33 @@ for guess in range(args.test_trials):
                                 bias_scaling=args.bias_scaling,
                                 ).to(device)
 
-        # ########### define the orthogonal branch ########### #
-        if args.ortho == 'random':
-            Ortog, _ = np.linalg.qr(2*np.random.rand(args.n_hid,args.n_hid)-1)
-        elif args.ortho == 'cycle':
-            # SimpleCycle 
-            Ortog = np.zeros((args.n_hid, args.n_hid))
-            Ortog[0, args.n_hid-1] = 1
-            for i in range(args.n_hid-1):
-                Ortog[i+1,i] = 1  
-        elif args.ortho == 'identity':
-            Ortog = np.eye(args.n_hid)
-        elif args.ortho == 'null':
-            Ortog = np.zeros(args.n_hid)
-        else:
-            raise ValueError('Only random, cycle, identity, and null, are supported for the orthogonal branch.')
-        
-        model.reservoir[0].net.ortho = torch.nn.Parameter(torch.Tensor(Ortog).to(device), requires_grad=False)
-        # #################################################### #
-
-        if args.simple_cycle:
-            # SimpleCycle reservoir 
-            W = np.zeros((args.n_hid, args.n_hid))
-            W[0, args.n_hid-1] = args.rho
-            for i in range(args.n_hid-1):
-                W[i+1,i] = args.rho  # Tino used 0.5 but with input in [-0.5,0.5] 
-            model.reservoir[0].net.recurrent_kernel = torch.nn.Parameter(torch.Tensor(W).to(device), requires_grad=False)
-
+    # ########### define the orthogonal branch ########### #
+    if args.ortho == 'random':
+        Ortog, _ = np.linalg.qr(2*np.random.rand(args.n_hid,args.n_hid)-1)
+    elif args.ortho == 'cycle':
+        # SimpleCycle 
+        Ortog = np.zeros((args.n_hid, args.n_hid))
+        Ortog[0, args.n_hid-1] = 1
+        for i in range(args.n_hid-1):
+            Ortog[i+1,i] = 1  
+    elif args.ortho == 'identity':
+        Ortog = np.eye(args.n_hid)
+    elif args.ortho == 'null':
+        Ortog = np.zeros(args.n_hid)
     else:
+        raise ValueError('Only random, cycle, identity, and null, are supported for the orthogonal branch.')
+        
+    model.reservoir[0].net.ortho = torch.nn.Parameter(torch.Tensor(Ortog).to(device), requires_grad=False)
+    # #################################################### #
 
-        model = coESN(n_inp, args.n_hid, args.dt, gamma, epsilon, args.rho,
-                    args.inp_scaling, device=device).to(device)
-        if args.check:
-            check_passed = check(model)
-            print("Check: ", check_passed)
-            if not check_passed:
-                raise ValueError("Check not passed.")
+    if args.simple_cycle:
+        # SimpleCycle reservoir 
+        W = np.zeros((args.n_hid, args.n_hid))
+        W[0, args.n_hid-1] = args.rho
+        for i in range(args.n_hid-1):
+            W[i+1,i] = args.rho  # Tino used 0.5 but with input in [-0.5,0.5] 
+        model.reservoir[0].net.recurrent_kernel = torch.nn.Parameter(torch.Tensor(W).to(device), requires_grad=False)
+
 
     #(train_dataset, train_target), (valid_dataset, valid_target), (test_dataset, test_target) = get_mackey_glass(args.lag)
 
@@ -209,10 +184,8 @@ for guess in range(args.test_trials):
     test_nmse = test_esn(test_dataset, test_target, classifier, scaler) if args.use_test else 0.0
     NRMSE[guess] = test_nmse
 
-    if args.no_friction: # coESN
-        f = open(f'{main_folder}/mackey_log_coESN.txt', 'a')
-    else: # ESN
-        f = open(f'{main_folder}/{namefile}.txt', 'a')
+      
+    f = open(f'{main_folder}/{namefile}.txt', 'a')
     ar = ''
     for k, v in vars(args).items():
         ar += f'{str(k)}: {str(v)}, '
@@ -230,9 +203,7 @@ lastprint = ' ##################################################################
 lastprint += 'Mean NRMSE ' + str(mean) + ',    std ' + str(std) + '\n'
 lastprint += ' ##################################################################### \n'
 print(lastprint)
-if args.no_friction: # coESN
-    f = open(f'{main_folder}/mackey_log_coESN.txt', 'a')
-else: # ESN
-    f = open(f'{main_folder}/{namefile}.txt', 'a')
+    
+f = open(f'{main_folder}/{namefile}.txt', 'a')
 f.write(lastprint)
 f.close()
